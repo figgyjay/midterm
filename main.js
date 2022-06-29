b0f1caa726b23b02e9adcf675c5f1344209cabea
glf30 <greyson.frazier@gmail.com>
2022-06-29 16:46:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -129,21 +129,3 @@ console.log("///////////////////////////////////////////////////////////////////
 
 console.log("\n////////////////////////////////////////////////////////////////////////////\n");
 
-
-/* 
-Write a function called twoSmallest that takes in a single array of numbers as a parameter and returns the smallest two numbers in the following array format where the smallest number always comes before the 2nd smallest number (ie, the bigger number should come second): 
-[smallestNumber, secondSmallestNumber]
-If the array is shorter than length 2, return undefined to the user
-You can assume that every number in the array will be unique
-*/
-
-
-/* console.logs to test */
-console.log("twoSmallest");
-console.log("////////////////////////////////////////////////////////////////////////////\n");
-//Add console.logs here to test!  Feel free to format this section however you like
-
-console.log("\n////////////////////////////////////////////////////////////////////////////\n");
-
-
-console.log("\n////////////////////////////////////////////////////////////////////////////\n");
